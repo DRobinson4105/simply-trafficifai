bee14b836a5aa8282e38cdca9034da82a98598bd
David Robinson <drobinson4105@gmail.com>
2025-09-27 23:49:04 -0400

    aaaaa

--- api/routes.py
+++ api/routes.py
@@ -5,16 +5,11 @@ import cv2
 from queue import Queue
 from ultralytics import YOLO
 import numpy as np
-from threading import Lock
+from threading import Lock, Thread
 import time
 import torch
 
-
-cv2.setNumThreads(1)
-torch.set_grad_enabled(False)
-inference_lock = Lock()
 device = 0 if torch.cuda.is_available() else "cpu"
-
 
 app = Flask(__name__)
 CORS(app)
@@ -35,13 +30,17 @@ model = YOLO("yolo11n.pt")
 camera_frames = [None, None, None]
 camera_locks = [Lock(), Lock(), Lock()]
 
+max_is_special = False
+
 @app.route('/api/build-path', methods=['POST'])
 def build_path():
+    global max_is_special
+    if max_is_special: return "", 200
+    max_is_special = True
     route = request.get_json()
 
     for point in route:
         closest = min(camera_data.keys(), key=lambda i: cv2.norm((camera_data[i]["latitude"], camera_data[i]["longitude"]), (point["latitude"], point["longitude"])))
-        # print(cv2.norm((camera_data[closest]["latitude"], camera_data[closest]["longitude"]), (point["latitude"], point["longitude"])))
 
         if cv2.norm((camera_data[closest]["latitude"], camera_data[closest]["longitude"]), (point["latitude"], point["longitude"])) < EPS and closest not in used_cameras:
             used_cameras.add(closest)
@@ -74,12 +73,10 @@ def first_mask_with_point(masks, point):
 
 @app.route('/api/update', methods=['PUT'])
 def update():
-    print(cameras.qsize())
+    if len(current) == 0: return "", 200
     data = request.get_json()
     latitude, longitude = float(data["latitude"]), float(data["longitude"])
     # if new camera is found, rotate current cameras and update to new data
-    # print((camera_data[current[0]["name"]]["latitude"], camera_data[current[0]["name"]]["longitude"]))
-    # print((latitude, longitude))
     if cv2.norm((camera_data[current[0]["name"]]["latitude"], camera_data[current[0]["name"]]["longitude"]), (latitude, longitude)) < EPS:
         current[0]["capture"].release()
 
@@ -96,104 +93,88 @@ def update():
             "capture": cv2.VideoCapture(f'videos/{name}.ts')
         }
 
-    # then, run another tracking frame for all current videos
-    for i in range(3):
-        global frame_count
-        # current[i]["capture"].set(cv2.CAP_PROP_POS_FRAMES, frame_count)
+    return "", 200
 
-        ret, frame = current[i]["capture"].read()
-        if not ret:
-            return "Frame failed", 400
-        
-        # print(1)
-        frame = np.ascontiguousarray(frame)
-        with inference_lock, torch.inference_mode():
-            results = model.predict(
-                source=frame,
-                device=device,
-                workers=0,      # no dataloader threads
-                stream=False,
-                verbose=False
-                # imgsz=640, conf=0.25, iou=0.45   # (optional)
-            )
-        # print(2)
+def main_loop():
+    while True:
+        if len(current) == 0: continue
 
-        boxes = results[0].boxes
-        
-        pairs = list(current[i]["id_map"].items())
-        ids, points = [x[0] for x in pairs], [x[1] for x in pairs]
-        new_id_map = {}
-        
-        n = len(boxes.cls)
+        for i in range(3):
+            ret, frame = current[i]["capture"].read()
+            if not ret:
+                return "Frame failed", 400
+            
+            results = model.predict(source=frame, device=device)
 
-        for j in range(n):
-            cls = int(boxes.cls[j].item())
-            if cls == 2:
-                cur = (boxes.xywh[j][0].item(), (boxes.xywh[j][1] + boxes.xywh[j][3] / 2).item())
+            boxes = results[0].boxes
+            
+            pairs = list(current[i]["id_map"].items())
+            ids, points = [x[0] for x in pairs], [x[1] for x in pairs]
+            new_id_map = {}
+            
+            n = len(boxes.cls)
 
-                if len(points) == 0:
-                    cur_id = current[i]["next_id"]
-                    current[i]["next_id"] += 1
-                else:
-                    closest_idx = min(range(len(points)), key=lambda a: cv2.norm(points[a], cur))
-                    
-                    if cv2.norm(points[closest_idx], cur) < 10:
-                        cur_id = ids[closest_idx]
-                    else:
+            for j in range(n):
+                cls = int(boxes.cls[j].item())
+                if cls == 2:
+                    cur = (boxes.xywh[j][0].item(), (boxes.xywh[j][1] + boxes.xywh[j][3] / 2).item())
+
+                    if len(points) == 0:
                         cur_id = current[i]["next_id"]
                         current[i]["next_id"] += 1
+                    else:
+                        closest_idx = min(range(len(points)), key=lambda a: cv2.norm(points[a], cur))
+                        
+                        if cv2.norm(points[closest_idx], cur) < 10:
+                            cur_id = ids[closest_idx]
+                        else:
+                            cur_id = current[i]["next_id"]
+                            current[i]["next_id"] += 1
 
-                new_id_map[cur_id] = cur
-                
-                lane = first_mask_with_point(camera_data[current[i]["name"]]["mask1"], cur)
-                dir = 1
+                    new_id_map[cur_id] = cur
+                    
+                    lane = first_mask_with_point(camera_data[current[i]["name"]]["mask1"], cur)
+                    dir = 1
 
-                if lane is None:
-                    lane = first_mask_with_point(camera_data[current[i]["name"]]["mask2"], cur)
-                    dir = -1
+                    if lane is None:
+                        lane = first_mask_with_point(camera_data[current[i]["name"]]["mask2"], cur)
+                        dir = -1
 
-                if lane is None: continue
+                    if lane is None: continue
 
-                if cur_id in current[i]["tracked"].keys():
-                    if current[i]["tracked"][cur_id][0] == -1 * dir * (lane + 1):
-                        current[i]["lane_speeds"][lane] += frame - current[i]["tracked"][cur_id][1]
-                        current[i]["lane_counts"][lane] += 1
-                        current[i]["tracked"].pop(cur_id)
-                        # print(f'{cur_id} crossed lane {lane} at frame {idx}')
-                        # print(f'end: {cur_id} {dir} {lane} {frame} {cur}')
-                else:
-                    # print(f'start: {cur_id} {dir} {lane} {frame} {cur}')
-                    current[i]["tracked"][cur_id] = (dir * (lane + 1), frame)
+                    if cur_id in current[i]["tracked"].keys():
+                        if current[i]["tracked"][cur_id][0] == -1 * dir * (lane + 1):
+                            current[i]["lane_speeds"][lane] += frame - current[i]["tracked"][cur_id][1]
+                            current[i]["lane_counts"][lane] += 1
+                            current[i]["tracked"].pop(cur_id)
+                            # print(f'{cur_id} crossed lane {lane} at frame {idx}')
+                            # print(f'end: {cur_id} {dir} {lane} {frame} {cur}')
+                    else:
+                        # print(f'start: {cur_id} {dir} {lane} {frame} {cur}')
+                        current[i]["tracked"][cur_id] = (dir * (lane + 1), frame)
 
-        annotated = results[0].plot()
+            annotated = results[0].plot()
 
-        contours = [np.array(m, dtype=np.int32).reshape((-1, 1, 2)) for m in camera_data[current[i]["name"]]["mask1"]]
-        cv2.polylines(annotated, contours, isClosed=True, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+            contours = [np.array(m, dtype=np.int32).reshape((-1, 1, 2)) for m in camera_data[current[i]["name"]]["mask1"]]
+            cv2.polylines(annotated, contours, isClosed=True, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
-        contours = [np.array(m, dtype=np.int32).reshape((-1, 1, 2)) for m in camera_data[current[i]["name"]]["mask2"]]
-        cv2.polylines(annotated, contours, isClosed=True, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+            contours = [np.array(m, dtype=np.int32).reshape((-1, 1, 2)) for m in camera_data[current[i]["name"]]["mask2"]]
+            cv2.polylines(annotated, contours, isClosed=True, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
-        ok, buf = cv2.imencode('.jpg', annotated)
-        
-        if not ok: 
-            return "Frame failed", 400
-        
-        jpg = buf.tobytes()
+            ok, buf = cv2.imencode('.jpg', annotated)
+            
+            if not ok: 
+                return "Frame failed", 400
+            
+            jpg = buf.tobytes()
 
-        with camera_locks[i]:
-            camera_frames[i] = jpg
+            with camera_locks[i]:
+                camera_frames[i] = jpg
 
-        # current[i]["id_map"] = new_id_map
-
-    frame += 1
-
-    # print('done')
-    return "", 200
+            current[i]["id_map"] = new_id_map
 
 def stream(idx):
     while True:
-        # while camera_frames[idx] == None:
-        #     print(camera_frames[idx])
         with camera_locks[idx]:
             jpg = camera_frames[idx]
         if jpg is not None:
@@ -216,12 +197,15 @@ def camera3():
 
 @app.route('/api/get-optimal-lanes', methods=['GET'])
 def get_optimal_lanes():
-    data = np.array(camera_data[current[0]["name"]]["average_speed"])
-    return jsonify((data - np.min(data)) / (np.max(data) - np.min(data))), 200
+    # data = np.array(camera_data[current[0]["name"]]["average_speed"])
+    # return jsonify(((data - np.min(data)) / (np.max(data) - np.min(data))).tolist()), 200
+    return [True, True, False], 200
 
 @app.route('/api/get-alert', methods=['GET'])
 def get_alert():
     return "", 200
 
 if __name__ == '__main__':
+    t = Thread(target=main_loop, daemon=True)
+    t.start()
     app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
